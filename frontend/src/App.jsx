62b5e55d044661ad0a9ebe02e98dccb6035c119e
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-04-13 01:36:28 +0530

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,26 @@
 import React from "react"
-
+import {Routes, Route} from "react-router-dom"
+import { Home } from "./pages/Home"
+import Collection from "./pages/Collection"
+import Cart from "./pages/Cart"
+import Product from "./pages/Product"
+import Contact from "./pages/Contact"
+import About from "./pages/About"
+import Login from "./pages/Login"
+import PlaceOrder from "./pages/PlaceOrder"
 const App = ()=>{
-  return (<div>
-
+  return (<div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/collection" element={<Collection/>}/>
+      <Route path="about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/product:productId" element={<Product/>}/>
+      <Route path="/cart" element={<Cart/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/place-order" element={<PlaceOrder/>}/>
+      <Route path="/orders" element={<Orders/>}/>
+    </Routes>
   </div>)
 }
 
