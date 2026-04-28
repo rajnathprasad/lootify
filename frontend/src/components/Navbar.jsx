eeeee526f0a6a9ffd918e772028c77399d0d93c4
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-04-28 13:48:02 +0530

    latest products

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { assets } from "../assets/assets";
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
+  const [LatestProducts, setLatestProducts] = useState([])
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <img src={assets.logo} className="w-36" alt="logo" />
@@ -56,7 +57,7 @@ const Navbar = () => {
       </div>
       {/* Sidebar menu for small screens */}
       <div
-        className={`absolute top-0 right-0 bittom-0 overflow-hiddden bg-white transition-all ${visible ? "w-full" : "w-0"}`}
+        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? "w-full" : "w-0"}`}
       >
         <div className="flex flex-col text-gray-600">
           <div onClick={()=>{setVisible(false)}} className="flex items-center gap-4 p-3">
